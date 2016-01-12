23d40cead5c515fcaf011dc279d324740f4dcf19
superbeef150 <mlgdotenterprises@gmail.com>
2016-01-12 23:11:13 +0000

    Third Test Complete

--- game.py
+++ game.py
@@ -1,5 +1,34 @@
 class Game(object):
-    score = 0
+    rolls = [0] * 21 #or you could do for i in xrange(21)]
+    currentRoll = 0
     
     def roll(self, pins):
-        self.score += pins
+        #self.score += pins
+        self.rolls[self.currentRoll] = pins
+        self.currentRoll = self.currentRoll + 1
+        
+    def score(self):
+        score = 0
+        frameIndex = 0
+        for frame in xrange(0, 10):
+            if (self.isSpare(frameIndex)):
+                score += 10 + self.rolls[frameIndex+2]
+            else:
+                score += self.rolls[frameIndex] + self.rolls[frameIndex+1]
+            frameIndex += 2
+        return score
+        
+    
+    def isSpare(self, frameIndex):
+        return self.rolls[frameIndex] + self.rolls[frameIndex+1] == 10
+
+
+'''
+For the last check-in for Test 2, it is worth noting that in python, you cannot have variables and functions with the same name.
+
+In the example, they had both an int and a function that returns an int called score
+In my check-in for Test 2 I did not include the function since the var score is necessary but the function is not
+If you rename the function to something such as getScore it flags it as unnecessary, so I simply left it out
+
+that having been said, the int variable "score" is replaced in a method in the steps taken to complete Test 3 so this becomes a moot point
+'''
--- testGame.py
+++ testGame.py
@@ -13,12 +13,23 @@ class BowlingGameTest(unittest.TestCase):
     def test_game_all_gutterballs(self):
         self.setUp()
         self.rollMany(20, 0)
-        self.assertEqual(0, self.g.score)
+        self.assertEqual(0, self.g.score())
         
     def test_game_all_1s(self):
         self.setUp()
         self.rollMany(20, 1)
-        self.assertEqual(20, self.g.score)
+        self.assertEqual(20, self.g.score())
+        
+    def test_one_spare(self):
+        self.setUp()
+        self.rollSpare()
+        self.g.roll(3)
+        self.rollMany(17, 0) #17 gutterballs ouch lol
+        self.assertEqual(16, self.g.score())
+        
+    def rollSpare(self):
+        self.g.roll(5)
+        self.g.roll(5)
 
 if __name__ == '__main__':
     unittest.main()
