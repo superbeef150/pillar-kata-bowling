52922458c79da4045b7231c82318f153fd7612a5
superbeef150 <mlgdotenterprises@gmail.com>
2016-02-21 08:46:43 +0000

    Added another test based on IRL game

--- testGame.py
+++ testGame.py
@@ -69,6 +69,32 @@ class BowlingGameTest(unittest.TestCase):
         self.g.roll(9)
         self.g.roll(6)
         self.assertEqual(101, self.g.score())
+        
+    def test_John_IRL(self):
+        self.setUp()
+        print "test_John_IRL"
+        self.g.roll(3)
+        self.g.roll(5)
+        self.g.roll(0)
+        self.g.roll(8)
+        self.g.roll(1)
+        self.g.roll(8)
+        self.rollStrike()
+        self.g.roll(4)
+        self.g.roll(0)
+        self.g.roll(1)
+        self.g.roll(1)
+        self.g.roll(6)
+        self.g.roll(4)#spare
+        self.g.roll(0)
+        self.g.roll(8)
+        self.g.roll(9)
+        self.g.roll(1)#spare
+        self.g.roll(5)
+        self.g.roll(5)#spare
+        self.g.roll(5)
+        self.assertEqual(93, self.g.score())
+        
     
     def rollStrike(self):
         self.g.roll(10)
